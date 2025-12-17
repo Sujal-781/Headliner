762a904eadb46edb4e4a5b8629835b978b1a7c3a
Sujal Choudhary <chsujal8@gmail.com>
2025-12-17 18:41:23 +0530

    Added annotation for caching setup

--- headliner-backend/src/main/java/com/projs/FeedFocusBackend/HeadlinerBackendApplication.java
+++ headliner-backend/src/main/java/com/projs/FeedFocusBackend/HeadlinerBackendApplication.java
@@ -2,10 +2,12 @@ package com.projs.FeedFocusBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableCaching
 public class HeadlinerBackendApplication {
 
 	public static void main(String[] args) {
